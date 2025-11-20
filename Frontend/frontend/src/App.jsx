4447dd1a864e6c4cffae18596468409d7e8475a8
jigyash <realjigyash19@gmail.com>
2025-11-20 22:04:51 +0530

    dummy

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -5,13 +5,11 @@ import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-slate-900">
-      <div className="container mx-auto px-4">
-        <NavBar/>
+    <div className="min-h-screen bg-slate-900 text-white">
+
         <Routes>
           <Route path='/' element={<LandingPage/>}/>
         </Routes>
-      </div>
     </div>
   );
 };
--- Frontend/frontend/src/pages/LandingPage.jsx
+++ Frontend/frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import NavBar from '../components/NavBar'
 const LandingPage = () => {
     return ( 
-        <div className="text-blue-500 font-semibold text-3xl">
+        <div className="relative">
+        <NavBar/>
+         <div className="text-blue-500 font-semibold text-3xl">
             Welcome to the Landing Page 
+        </div>           
         </div>
+
     )
 }
 export default LandingPage;
