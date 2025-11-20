e3a6488e828b8a5388aa6d1d2018c080aa801caa
jigyash <realjigyash19@gmail.com>
2025-11-20 07:39:50 +0530

    ui updated

--- Frontend/frontend/src/App.jsx
+++ Frontend/frontend/src/App.jsx
@@ -1,55 +1,19 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
-import CardNav from './components/CardNav'
+import NavBar from './components/NavBar'
+
 const App = () => {
-  const items = [
-    {
-      label: "About",
-      bgColor: "#0D0716",
-      textColor: "#fff",
-      links: [
-        { label: "Company", ariaLabel: "About Company" },
-        { label: "Careers", ariaLabel: "About Careers" }
-      ]
-    },
-    {
-      label: "Projects", 
-      bgColor: "#170D27",
-      textColor: "#fff",
-      links: [
-        { label: "Featured", ariaLabel: "Featured Projects" },
-        { label: "Case Studies", ariaLabel: "Project Case Studies" }
-      ]
-    },
-    {
-      label: "Contact",
-      bgColor: "#271E37", 
-      textColor: "#fff",
-      links: [
-        { label: "Email", ariaLabel: "Email us" },
-        { label: "Twitter", ariaLabel: "Twitter" },
-        { label: "LinkedIn", ariaLabel: "LinkedIn" }
-      ]
-    }
-  ];
   return (
-<>
-    <CardNav
-      
-      logoAlt="Company Logo"
-      items={items}
-      baseColor="#fff"
-      menuColor="#000"
-      buttonBgColor="#111"
-      buttonTextColor="#fff"
-      ease="power3.out"
-    />
-    <Routes>
-      <Route path='/' element={<LandingPage/>}/>
-    </Routes>
-</>
-  )
-}
+    <div className="min-h-screen bg-slate-900">
+      <div className="container mx-auto px-4">
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App
